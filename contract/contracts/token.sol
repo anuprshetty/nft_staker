f65be9362fc8b53d5e84e8395a6284f1ad0f558d
anuprshetty <anuprshetty@gmail.com>
2022-08-19 23:53:22 +0530

    token contract

---
+++ contract/contracts/token.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    uint256 public immutable maxSupply;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _maxSupply
+    )
+        ERC20(
+            bytes(_name).length > 0 ? _name : "TomAndJerry Token",
+            bytes(_symbol).length > 0 ? _symbol : "TNJT"
+        )
+    {
+        maxSupply = _maxSupply * 1 ether;
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(
+            (totalSupply() + amount) <= maxSupply,
+            "Maximum supply has been reached"
+        );
+        _mint(to, amount);
+    }
+}
