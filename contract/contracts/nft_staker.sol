126bf53ed2947055d97b8e0b5ca37ab491567363
anuprshetty <anuprshetty@gmail.com>
2022-10-16 23:49:48 +0530

    rewardIntervals in nft_staker contract

--- contract/contracts/nft_staker.sol
+++ contract/contracts/nft_staker.sol
@@ -5,4 +5,15 @@ pragma solidity ^0.8.0;
 import "./nft_minter.sol";
 import "./nft_reward.sol";
 
-contract NFTStaker is Ownable, IERC721Receiver {}
+contract NFTStaker is Ownable, IERC721Receiver {
+    enum RewardIntervalType {
+        per_second,
+        per_min,
+        per_hour,
+        per_day,
+        per_month,
+        per_year
+    }
+
+    mapping(RewardIntervalType rewardIntervalType => uint256 rewardInterval)
+        public rewardIntervals;
